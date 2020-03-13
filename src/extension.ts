f2a9957694ed16cc67fcc3b9bf43484312e9500b
Nathachai Thongniran <inid3a@gmail.com>
2020-03-13 13:43:19 +0700

    feat: add extension.openVuePdf command

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as path from 'path'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function openImageWebview (title: string, assetPath: string, imageNames: string[]) {
+  // set panel
+  const viewType: string = title.replace(/\s/g, '')
+  const panel = vscode.window.createWebviewPanel(viewType, title,
+    vscode.ViewColumn.Active, {
+      localResourceRoots: [vscode.Uri.file(assetPath)],
+      enableScripts: false
+    }
+  )
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vue-cheatsheet" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  // set content
+  let html: string = ''
+  for (const imageName of imageNames) {
+    const filePath = vscode.Uri.file(path.join(assetPath, imageName)).with({ scheme: 'vscode-resource' })
+    html += `<img src="${filePath}" />`
+  }
+  panel.webview.html = html
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function activate (context: vscode.ExtensionContext) {
+  const assetPath = path.join(context.extensionPath, 'asset')
+
+  const vueDisposable = vscode.commands.registerCommand('extension.openVuePdf', () => {
+    openImageWebview('Vue Cheatsheet Vue', assetPath, [
+      'Vue-Essentials-Cheat-Sheet-1.jpg',
+      'Vue-Essentials-Cheat-Sheet-2.jpg'
+    ])
+  })
+
+  context.subscriptions.push(vueDisposable)
+}
+
+export function deactivate () {}
